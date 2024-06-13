d21469436c271b7c3ce092396a95d96da6e2f238
KadirQuintero03 <kadireq1928@gmail.com>
2024-06-12 21:48:25 -0500

    Cargados los datos personales del usuario loggeado en su ranking

--- lib/ui/views/home/statistics_screen.dart
+++ lib/ui/views/home/statistics_screen.dart
@@ -1,8 +1,30 @@
-import 'package:edupro/shared/widgets/nav/navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:edupro/shared/widgets/nav/navigation_bar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class StatisticsPage extends StatelessWidget {
+class StatisticsPage extends StatefulWidget {
   const StatisticsPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _StatisticsPage createState() => _StatisticsPage();
+}
+
+class _StatisticsPage extends State<StatisticsPage> {
+  String _userName = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  Future<void> _loadUserData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _userName = prefs.getString('userName') ?? 'Sin nombre';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,26 +43,27 @@ class StatisticsPage extends StatelessWidget {
             ),
             const SizedBox(
                 height: 20.0), // Espacio entre la imagen y los textos
-            const Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                CircleAvatar(
+                const CircleAvatar(
                   radius: 30.0,
                   backgroundImage: AssetImage('assets/images/michael.jpg'),
                 ),
-                SizedBox(width: 16.0), // Espacio entre la imagen y el texto
+                const SizedBox(
+                    width: 16.0), // Espacio entre la imagen y el texto
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      'Juan Peralta',
-                      style: TextStyle(
+                      _userName,
+                      style: const TextStyle(
                         fontSize: 18.0,
                         fontWeight: FontWeight.bold,
                         color: Colors.black,
                       ),
                     ),
-                    Text(
+                    const Text(
                       'Bienvenido al ranking semanal',
                       style: TextStyle(
                         fontSize: 14.0,
